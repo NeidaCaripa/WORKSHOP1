99f157ab6af64752210ef4e70f0b8ab96b97d8a6
ASernaLopez <alejo21625@gmail.com>
2023-08-09 11:03:06 -0500

    Validación de usuarios

--- pages/admin/admin.js
+++ pages/admin/admin.js
@@ -1,4 +1,42 @@
 import { getProducts } from "./conexionapi.js";
+
+
+const inputEmail = document.getElementById("email")
+const inputPassword = document.getElementById("password")
+const form = document.getElementById("formulario")
+
+const usuarios= [
+  {
+    correo: "neida.caripa@gmail.com",
+    contrasena: 628223
+  },
+  { 
+    correo: "alejo.serna@gmail.com",
+    contrasena: 123456
+  }];
+
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+  
+    const emailValue = inputEmail.value;
+    const passwordValue = inputPassword.value;
+  
+    const usuarioEncontrado = usuarios.find(usuario => usuario.correo === emailValue 
+      && usuario.contrasena.toString() === passwordValue);
+  
+    if (usuarioEncontrado) {
+      listProducts(currentCollection);
+      form.style.display = "none";
+      alert("Inicio de sesión exitoso");
+    } else {
+      alert("Información incorrecta");
+    }
+  });
+  
+  
+
+
 const card = document.querySelector(".shoes-container");
 
 const url = window.location.search;
@@ -26,4 +64,5 @@ async function listProducts(collection) {
   });
 }
 
-listProducts(currentCollection);
+
+
