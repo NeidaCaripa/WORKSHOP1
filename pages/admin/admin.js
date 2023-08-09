2e993fe1cc4b1ab4c9a464136b851972ea85482b
Neida Caripa <neida.caripa@gmail.com>
2023-08-09 18:58:52 -0500

    Agregar y Eliminar producto

--- pages/admin/admin.js
+++ pages/admin/admin.js
@@ -1,5 +1,17 @@
 import { getProducts } from "./conexionapi.js";
 
+const formAdd = document.getElementById("form")
+const nombreProducto = document.getElementsByName('nombre')[0]; // Accedemos al primer elemento
+const precioProducto = document.getElementsByName('precio')[0];
+const tipoColleccion = document.getElementsByName('coleccion')[0];
+const imagen0 = document.getElementsByName('imagen0')[0];
+const imagen1 = document.getElementsByName('imagen1')[0];
+const imagen2 = document.getElementsByName('imagen2')[0];
+const imagen3 = document.getElementsByName('imagen3')[0];
+const imagen4 = document.getElementsByName('imagen4')[0];
+const addForm = document.getElementById('addForm')
+const btnNew = document.getElementById('btnNew');
+
 
 const inputEmail = document.getElementById("email")
 const inputPassword = document.getElementById("password")
@@ -28,6 +40,7 @@ const usuarios= [
     if (usuarioEncontrado) {
       listProducts(currentCollection);
       form.style.display = "none";
+      btnNew.style.display= "block"
       alert("Inicio de sesión exitoso");
     } else {
       alert("Información incorrecta");
@@ -48,21 +61,69 @@ async function listProducts(collection) {
   products.forEach((product) => {
     if (collection === null || product.coleccion === collection) {
       const template = `
-        <a class="shoe-card" href="productDetail.html?id=${product.id}">
+        <article class="shoe-card">
             <li>
                 <img class="shoe-image" src="${product.imagen0}" alt="${product.nombre}" />
                 <h2 class="shoe-name">${product.nombre}</h2>
                 <p class="shoe-collection">${product.coleccion}</p>
                 <p class="shoe-price">${product.precio} COP</p>
             </li>
-            <span>Editar</span>
-            <span>Eliminar</span>
-        </a>
+            <button class="btnDelete" name="${product.id}">Eliminar</button>
+        </article>
         `;
       card.innerHTML += template;
     }
   });
+
+  const btnDelete = document.getElementsByClassName('btnDelete');
+
+  Array.from(btnDelete).forEach((element) => {
+    let id = element.getAttribute('name');
+    element.addEventListener('click', () => {
+      handleDelete(id);
+    })
+  });
 }
 
+const URL = 'http://localhost:3000';
 
+const handleDelete = async (id) => {
 
+  try {
+    const response = await axios.delete(`${URL}/productos/${id}`);
+    console.log('Producto eliminado:', response.data);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+  getProducts();
+}
+
+const addProducto = async (e) => {
+  e.preventDefault()
+    const updatedData = {
+      "nombre": nombreProducto.value,
+      "precio": precioProducto.value,
+      "coleccion": tipoColleccion.value,
+      "imagen0": imagen0.value,
+      "imagen1": imagen1.value,
+      "imagen2": imagen2.value,
+      "imagen3": imagen3.value,
+      "imagen4": imagen4.value
+    };
+  
+    try {
+      const response = await axios.post(`${URL}/productos`, updatedData);
+      console.log('Producto agreado:', response.data);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  
+  }
+  formAdd.addEventListener('submit', addProducto)
+  
+  const showForm = () =>{
+    addForm.style.display= "block"
+  }
+  btnNew.addEventListener('click', showForm);
